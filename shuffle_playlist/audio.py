b6e51b4e85743ec9e27be8ee3fef139bbf4ac5f8
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2022-12-12 17:53:09 -0600

    Use the factory method in audio.py to abstract tag parsing
    Function get_tags() is responsible for creating a
    tag parser object which has the get_tags() and
    get_info() methods.

--- shuffle_playlist/audio.py
+++ shuffle_playlist/audio.py
@@ -1,9 +1,13 @@
 """Audio functions and classes."""
 
+# Ogg Vorbis
+from mutagen.oggvorbis import OggVorbis, OggVorbisInfo
+from mutagen._vorbis import VCommentDict
+
 from soundfile import SoundFile
-from mutagen.oggvorbis import OggVorbis
 from pathlib import PurePath
-from typing import Protocol
+from typing import Protocol, Union, cast
+import re
 
 class ExSoundFile(SoundFile):
     @property
@@ -11,20 +15,56 @@ class ExSoundFile(SoundFile):
         """Length in seconds."""
         return float(self.frames * self.channels) / float(self.samplerate)
 
-def get_tags(filename: str) -> OggVorbis:
-    """Get meta tags for the given FILENAME."""
-    return OggVorbis(filename)
+class TagParser(Protocol):
+    """A protocol for defining tag objects."""
 
-AUDIO_FACTORIES = {
-    'ogg': OggVorbis
+    def get_tags(self) -> dict:
+        """
+        Returns a dictionary with some metadata on the file.
+
+        Possible keys are:
+            * title (str)
+            * album (str)
+            * artist (str)
+        """
+        ...
+
+    def get_info(self) -> dict:
+        """
+        Returns a dictionary with information about the file.
+
+        Possible keys are:
+            * length (float) - length in seconds
+            * channels (int) - number of audio channels
+            * samplerate (int) - samplerate in hertz
+            * bitrate (int) - audio bitrate
+        """
+        ...
+
+class OggTagParser:
+    def __init__(self, filename: str):
+        self.__ogg = OggVorbis(filename)
+
+    def get_tags(self) -> dict:
+        tags: VCommentDict = cast(VCommentDict, self.__ogg.tags)
+        res = {k:v for (k,v) in tags}
+        return res
+
+    def get_info(self) -> dict:
+        info: OggVorbisInfo = cast(OggVorbisInfo, self.__ogg.info)
+        res = {
+            'length': info.length,
+            'channels': info.channels,
+            'samplerate': info.sample_rate,
+            'bitrate': info.bitrate
+        }
+        return res
+
+AUDIO_TAG_FACTORIES = {
+    'ogg': OggTagParser
 }
 
-class TagsFactory(Protocol):
-    """"""
-
-    def __init__(self, filename: str):
-        """"""
-        ...
+_ext_re = re.compile(r'(?i)\.([a-z0-9]+)')
 
 def get_file_dict(filename: str) -> dict:
     """
@@ -32,35 +72,51 @@ def get_file_dict(filename: str) -> dict:
 
     The returned dictionary contains the following:
         * length (float) - the length of the song in seconds
-        * title (str, None) - the title of the song, or None if unavailable
         * filename (str) - the name of the file with the leading path removed
+        * samplerate (float) - the samplerate of the file
+        * channels (int) - the number of channels
     """
-    ext = str(PurePath(filename).suffix)
+    res = {}
 
-    try:
-        TagClass = AUDIO_FACTORIES[ext]
-    except KeyError:
-        
-
-    ogg = ExSoundFile(filename)
-    res = {
-        'length': ogg.seconds,
-        'filename': ogg.name,
-        'title': None
-    }
-    oggtags = get_tags(filename)
-    oggtagslist = oggtags.keys()
-
-    # Get title
-    try:
-        title = oggtags['title']
-    except:
-        temp = PurePath(res['filename']).stem
-        title = str(temp)
-
-    if isinstance(title, list):
-        title = ' '.join(title)
-
-    res['title'] = title
+    with ExSoundFile(filename) as snf:
+        res['length'] = snf.seconds
+        res['filename'] = snf.name
+        res['samplerate'] = snf.samplerate
+        res['channels'] = snf.channels
 
     return res
+
+def get_tags(filename: str) -> TagParser:
+    """
+    Return the metadata tags for FILENAME.
+
+    FILENAME can be a str or bytes.
+
+    The returned object is a working implementation
+    that fits the structure of `TagParser`.
+
+    Raises:
+        ValueError
+            - if the file has no extension
+            - if the extension is not supported
+    """
+    m = _ext_re.search(filename)
+    if m is None:
+        raise ValueError(f"invalid filename '{filename}': missing extension")
+    ext = m[1]
+
+    cls = AUDIO_TAG_FACTORIES.get(ext)
+    if cls is None:
+        raise ValueError(f"invalid extension '{ext}'", filename)
+    res: TagParser = cls(filename)
+
+    return res
+
+__all__ = [
+    # Classes
+    'TagParser',
+
+    # Functions
+    'get_tags',
+    'get_file_dict'
+]
