c419696fb8717c26cc678d095a12def71e9a28a7
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2022-12-11 19:16:45 -0600

    Refactor utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,11 @@
-# Utility functions
+"""
+Utility functions and classes.
+
+Functions:
+    * shuffle_playlist()
+"""
+
+from typing import Protocol, Union, runtime_checkable
 import random
 
 class CommandlineError(RuntimeError):
@@ -7,6 +14,7 @@ class CommandlineError(RuntimeError):
 class StringInputError(RuntimeError):
     pass
 
-def shuffle_list(entries: list):
-    random.shuffle(entries)
-    return entries
+def shuffle_list(values: list) -> list:
+    """Shuffles the items in a list."""
+    random.shuffle(values)
+    return values
