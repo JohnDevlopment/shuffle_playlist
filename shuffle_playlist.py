c4027a5f4ad1e8b47eab7be22df5b0ad7838c7f7
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2022-12-11 20:06:12 -0600

    Simplify write_playlist in main module
    It does not check commandline arguments, nor
    does it modify any of the playlist data.

--- shuffle_playlist.py
+++ shuffle_playlist.py
@@ -5,55 +5,24 @@ from exwidgets.entry import ExEntry
 from exwidgets.constants import *
 from pathlib import Path
 from utils import *
-from audio import get_file_dict, get_tags
 import sys, playlists as pl
 
-def write_playlist(files: list, title, output):
+def write_playlist(files: list, titleVar: StringVar, outputVar: StringVar):
     """Writes the playlist to file."""
     # Params
      # Title
-    title = title.get()
+    title: str = titleVar.get()
     if len(title) == 0 or title.isspace():
         raise StringInputError("Title is empty")
 
      # Output file
-    output = output.get()
+    output: str = outputVar.get()
     if len(output) == 0 or output.isspace() or not output.endswith('.m3u'):
         raise StringInputError("Output file is empty or lacks the .m3u extension")
 
-    # No file arguments
-    if len(files) == 0:
-        raise CommandlineError("No files were provided")
-
-    entries = []
-
-    # Regular expression to turn things like
-    # "Title - Subtitle" into "Title: Subtitle"
-    artistsep_re = re.compile(r'\s*-\s*')
-
-    # Gather data from files
-    for path in files:
-        fields = get_file_dict(str(path))
-        
-        # Parsing the fields to make them better
-        fields['length'] = round(fields['length'])
-        fields['title'] = artistsep_re.sub(': ', fields['title'])
-        fields['filename'] = fields['filename'].replace(' ', '%20')
-        
-        # Add to list
-        entries.append(fields)
-
-    # Randomize list
-    entries = shuffle_list(entries)
-
-    # Write playlist data to file
-    with open(output, 'wt') as fd:
-        fd.write(f"#EXTM3U\n#PLAYLIST:{title}\n")
-        for entry in entries:
-            entry.update(get_tags(entry['filename'].replace('%20', ' ')))
-            artist = entry.get('artist')
-            entry['artist'] = f"{artist} - " if artist is not None else ''
-            fd.write("#EXTINF:{length},{artist}{title}\n{filename}\n".format(**entry))
+    # Gather entry list
+    playlist = pl.M3UPlaylist(shuffle_list(files))
+    pl.write_playlist(playlist, output)
 
     Root.destroy()
 
