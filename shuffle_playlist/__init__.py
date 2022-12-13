2fd246e5619460f33f11e2728f4791bfe138dd2f
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2022-12-13 16:07:18 -0600

    Add docstring and version-string to package

---
+++ shuffle_playlist/__init__.py
@@ -0,0 +1,8 @@
+"""
+Create a playlist where the files are shuffled around.
+
+To use this package, either run it directly via ``python3 -m shuffle_playlist``
+or use the standalone tool of the same name.
+"""
+
+__version__ = "1.0a1"
