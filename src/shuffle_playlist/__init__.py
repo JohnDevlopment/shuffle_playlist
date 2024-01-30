d366d8e2a13ca029e7844d73ae36d4205d92bd6f
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-01-30 02:46:47 -0600

    Bump version to 1.0-alpha2

--- src/shuffle_playlist/__init__.py
+++ src/shuffle_playlist/__init__.py
@@ -1,8 +1,3 @@
-"""
-Create a playlist where the files are shuffled around.
+"""Create a playlist where the files are shuffled around."""
 
-To use this package, either run it directly via ``python3 -m shuffle_playlist``
-or use the standalone tool of the same name.
-"""
-
-__version__ = "1.0a1"
+__version__ = "1.0a2"
